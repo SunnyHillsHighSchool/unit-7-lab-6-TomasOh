516ddb13c786fb1836dced5aec7c26ba7eff51d3
Tomas Oh <600018491@fjuhsd.org>
2021-02-20 02:16:24 +0000

    Added Unit 7 Lab 6

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Tomas Oh
 
 import static java.lang.System.*;
 
@@ -8,5 +8,20 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+    Word one = new Word("chicken");
+    out.println(one);
+    out.println("num vowels == " + one.getNumVowels());
+    out.println("num chars == " + one.getLength());
+
+
+    one.setWord("alligator");
+    out.println("\n\n"+one);
+    out.println("num vowels == " + one.getNumVowels());
+    out.println("num chars == " + one.getLength());
+
+    one.setWord("elephant");
+    out.println("\n\n"+one);
+    out.println("num vowels == " + one.getNumVowels());
+    out.println("num chars == " + one.getLength()); 
 	}
 }
--- Word.java
+++ Word.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Tomas Oh
 
 import static java.lang.System.*;
 
@@ -10,12 +10,64 @@ public class Word
    																//why is it static?
    
    //add an instance variable
+   private String s;
 
-	//create a constructor
-	
-	//create other methods
-	//look at the sample runner code
-	//on the lab handout
+	//create 2 constructor
+  public Word()
+  {
+    s = "";
+  }
+
+  public Word(String str)
+  {
+    s = str;
+  }
+
+  //setter method
+  public void setWord(String r)
+  {
+    s = r;
+  }
+
+  //getter method
+  public String getWord()
+  {
+    return s;
+  }
+
+  //getNumVowels
+  public int getNumVowels()
+  {
+   //Create an int variable called numVowels and set it equal = 0
+   int numVowels = 0;
+
+   //Create a loop to iterate through all of the characters in the word passed (int x)
+   for(int x = 0; x < s.length(); x++)
+   {
+     //Create an if statement to check if a word at position x has a vowel
+     if(vowels.contains(s.charAt(x) + ""))
+     {
+       //If true, then set numVowels + 1
+       numVowels++;
+     }
+   }
+   //Close if and loop, return numVowels
+   return numVowels;
+
+  }
+
+  //getLength
+  public int getLength()
+  {
+    //create an int variable called numChars and set it equal to the number of characters in the word, using length() method.
+    int numChars = s.length();
+    
+    return numChars;
+  }
 
 	//create a toString method
+  public String toString()
+  {
+    return "" + s;
+  }
 }
